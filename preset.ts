38cdd3be693f4b3d196d4ee4f74a19339387b39c
Rafael Camara <rafaelcamaram@gmail.com>
2021-04-09 16:13:13 -0300

    Adding initial files

---
+++ preset.ts
@@ -0,0 +1,11 @@
+import { Preset, color } from 'apply';
+
+Preset.setName('svelte-add/svelte-skeleton');
+
+Preset.editJson('package.json')
+  .merge({
+    name: 'name-changed',
+  })
+  .withTitle('Updating package project title');
+
+Preset.instruct(`Have ${color.magenta('fun')}!`);
